40d86f815524575e08130d098dfe70262fd5b4ec
Declan Whelan <declan@leanintuit.com>
2017-06-25 23:25:57 -0400

    Updated response validation step to handle either a json document or a table.

--- support/response-steps.js
+++ support/response-steps.js
@@ -1,12 +1,24 @@
 'use strict';
 
+const _ = require('lodash');
+
 require('chai').should();
 
 const {defineSupportCode} = require(process.cwd() + '/node_modules/cucumber');
 
 defineSupportCode(function({Then}) {
-  Then('the response should be', function (json) {
-    this.response.body.should.eql(JSON.parse(json));
+  Then(/^the response should be$/, function (json) {
+    if (_.isFunction(json)) {
+      throw new Error("missing a data table for the expected response. Try adding a data table!");
+    } else if (_.isString(json)) {
+      this.response.body.data.should.eql(JSON.parse(json));
+    } else {
+      _.each(json.hashes(), (row, index) => {
+        _.each(row, (value, key) => {
+          this.assertValue(`[${index}].` + _.camelCase(key), parseInt(value));
+        });
+      });
+    }
   });
 
   Then(/^the response status should be "([1-5]\d\d)"$/, function(status) {
