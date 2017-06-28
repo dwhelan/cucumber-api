b2fa34b8ea83c90288c7dcc944b9f490cfd6f1ef
Declan Whelan <declan@leanintuit.com>
2017-06-28 14:01:42 -0400

    Use chai-son-equal for checking JSON equality.

--- features/steps/request-steps.js
+++ features/steps/request-steps.js
@@ -1,11 +1,14 @@
 'use strict';
 
-require('chai').should();
+const chai = require('chai');
+const chaiJsonEqual = require('chai-json-equal');
+chai.use(chaiJsonEqual);
+chai.should();
 
 const {defineSupportCode} = require(process.cwd() + '/node_modules/cucumber');
 
 defineSupportCode(function({When, Then}) {
   Then('the request should be', function (json) {
-    JSON.parse(JSON.stringify(this.request)).should.eql(JSON.parse(json));
+    this.request.should.jsonEqual(JSON.parse(json));
   });
 });
